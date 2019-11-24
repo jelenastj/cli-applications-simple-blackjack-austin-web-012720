f0f1e4e37cc2028931d19de541277cc0f0de3915
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-24 17:16:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,60 @@
 def welcome
+  puts "Welcome to the Blackjack Table"
   # code #welcome here
 end
 
 def deal_card
+  rand(1..11)
   # code #deal_card here
 end
 
-def display_card_total
+def display_card_total (total)
+  puts "Your cards add up to #{total}"
   # code #display_card_total here
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
+ user_input = gets.chomp
   # code #get_user_input here
 end
 
-def end_game
+def end_game (card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
   # code #end_game here
 end
 
 def initial_round
-  # code #initial_round here
+ first_card = deal_card
+ second_card = deal_card
+ card_total = first_card+second_card
+ display_card_total(card_total)
+ return card_total
+ # code #initial_round here
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  user_input =get_user_input
+  if user_input =="h"
+    card_total += deal_card
+
+    elsif user_input =="s"
+    return card_total
+  else
+    invalid_command
+  end
+  prompt_user
+  get_user_input
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  
 end
 
 #####################################################
@@ -39,6 +62,9 @@ end
 #####################################################
 
 def runner
+  welcome
+  
+  
   # code runner here
 end
     
